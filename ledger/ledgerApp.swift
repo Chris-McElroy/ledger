433db03a7d64a23b8927eff6737a8a6493cb7e12
ChrisMcElroy <csmcelroy@me.com>
2022-11-05 13:51:44 +0000

    Initial Commit

---
+++ ledger/ledgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ledgerApp.swift
+//  ledger
+//
+//  Created by Chris McElroy on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ledgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
